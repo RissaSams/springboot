51a450bdc9dc2252e7412217e2c672ae213107a5
RissaSams <rissa.sams@yahoo.com>
2020-09-17 09:49:13 -0500

    Initial commit

---
+++ src/test/java/com/codejava/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codejava.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
